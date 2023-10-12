028d30372e382cab5eeb918d4cf8c861e65cc848
Antonio Caricati <140312553+TonyC98@users.noreply.github.com>
2023-10-12 15:09:38 +0700

    imported mongoose

--- index.js
+++ index.js
@@ -11,3 +11,27 @@ app.listen(4000, () => {
     console.log("server open")
 })
 
+//import mongodb and configure
+// const {MongoClient} = require('mongodb')
+// const client = new MongoClient(
+//     'mongodb+srv://antocaricati98:7yj0nEzIuUIC7G6r@cluster0.zhl5dbi.mongodb.net/cosplaying'
+// )
+// const db = client.db('cosplaying')
+
+//importing mongoose
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://antocaricati98:7yj0nEzIuUIC7G6r@cluster0.zhl5dbi.mongodb.net/cosplaying')
+
+//configure collections
+const Users = db.collection('users')
+
+//use route
+app.post('/users', async (req,res) => {
+    try {
+    let user = await Users.create(req.body)
+    res.send(user)
+    } catch (err) {
+        res.err(err)
+    }
+})
+    
