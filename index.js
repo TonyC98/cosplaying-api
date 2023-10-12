c0bbdf61020ef3a7ea6e6efa3e7b04c9f269ef80
Antonio Caricati <140312553+TonyC98@users.noreply.github.com>
2023-10-12 18:38:40 +0700

    added structure all routes

--- index.js
+++ index.js
@@ -39,3 +39,120 @@ app.post('/signup', async (req, res) => {
     }
 })
     
+//login route
+app.post('/login', async (req,res) => {
+    try {
+        let user = await Users.findOne(req.body)
+        res.send(user)
+        console.log(user)
+    } catch (err) {
+        res.send(err)
+        console.log(err);
+    }
+})
+
+//confirmation route
+app.get('/orders:id', async (req,res) => {
+    try {
+        let confirmation = await Orders.findById(req.query.id)
+        console.log(confirmation)
+    } catch (err) {
+        res.send(err)
+        console.log(err);
+    }
+})
+
+//all products route
+app.get('/products', async (req, res) => {
+    try {
+        let products = await Products.find(req.query)
+        res.send(products)
+    } catch (err) {
+        res.send(err)
+        console.log(err);
+    }
+})
+
+//search product route ------ to be confirmed
+app.get('/products', async (req,res) => {
+    try {
+        let filteredProducts = await Products.find({ name: { $regex: 'req.query', $options: 'i' } })
+        res.send(filteredProducts)
+        console.log(filteredProducts)
+    } catch (err) {
+        res.send(err)
+        console.log(err)
+    }
+})
+
+//get product route
+app.get('/products', async (req, res) => {
+    try {
+        let product = await Products.findById(req.query.id)
+        res.send(product)
+        console.log(product)
+    } catch (err) {
+        res.send(err)
+        console.log(err)
+    }
+})
+
+//checkout post route
+app.post('/orders', async (req, res) => {
+    try {
+        let order = await Orders.create(req.body)
+        res.send(order)
+        console.log(order)
+    } catch (err) {
+        res.send(err)
+        console.log(err)
+    }
+})
+
+//checkout get route
+app.get('/orders:id', async (req, res) => {
+    try {
+        let confirmedOrder = await Orders.findById(req.query)
+        res.send(confirmedOrder)
+        console.log(confirmedOrderrder)
+    } catch (err) {
+        res.send(err)
+        console.log(err)
+    }
+})
+
+//get profile route
+app.get('/profile', async (req, res) => {
+    try {
+        let profile = await Users.findById(req.query)
+        res.send(profile)
+        console.log(profile)
+    } catch (err) {
+        res.send(err)
+        console.log(err)
+    }
+})
+
+//new product route
+app.post('/products', async (req, res) => {
+    try {
+        let newProduct = await Products.createOne(req.body)
+        res.send(newProduct)
+            console.log(newProduct)
+    } catch (err) {
+        res.send(err)
+        console.log(err)
+    }
+})
+
+//edit product route
+app.patch('/products:id', async (req, res) => {
+    try {
+        let modifiedProduct = await Products.findByIdAndEdit(req.body)
+        console.log(modifiedProduct)
+        res.send(modifiedProduct)
+    } catch (err) {
+        res.send(err)
+        console.log(err)
+    }
+})
