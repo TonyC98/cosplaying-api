55513f28128ed7867a1fae2f56e41d9238731b28
Antonio Caricati <140312553+TonyC98@users.noreply.github.com>
2023-10-12 17:08:52 +0700

    added mongoose packages from airbnb project
    got rid of comments

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //import express
 const express = require('express')
+
 //express function
 const app = express()
 const bodyParser = require('body-parser')
@@ -13,17 +14,11 @@ app.get('/', (req, res) => {
     res.send('Welcome to this website')
     console.log("test test test");
 })
+
 //server open
 app.listen(4000, () => {
     console.log("server open")
 })
-
-//import mongodb and configure
-// const {MongoClient} = require('mongodb')
-// const client = new MongoClient(
-//     'mongodb+srv://antocaricati98:7yj0nEzIuUIC7G6r@cluster0.zhl5dbi.mongodb.net/cosplaying'
-// )
-// const db = client.db('cosplaying')
 
 //importing mongoose
 const mongoose = require('mongoose')
@@ -31,11 +26,6 @@ mongoose.connect('mongodb+srv://antocaricati98:7yj0nEzIuUIC7G6r@cluster0.zhl5dbi
 
 //models
 const Users = require('./models/Users')
-
-//configure collections
-// const Users = db.collection('users')
-
-//main route
 
 //use route
 app.post('/signup', async (req, res) => {
